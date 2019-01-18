85770ade787bad392b79b2bf070ddbc251fecc31
deepkanwal <deepkanwal@gmail.com>
2019-01-17 20:37:35 -0800

    Initial commit.

---
+++ pages/about.jsx
@@ -0,0 +1,10 @@
+import Header from '../components/Header.jsx';
+import Layout from '../components/MyLayout.jsx';
+
+export default () => (
+	<div>
+		<Layout>
+			<p>This is the about page</p>
+		</Layout>
+	</div>
+);
